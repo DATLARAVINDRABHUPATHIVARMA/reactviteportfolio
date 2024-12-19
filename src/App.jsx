2a0fafa45d73410e03e187c3ef2106941e261a4d
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2024-12-19 10:18:28 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Home from './Components/Home/Home.jsx'
+
 
 
 function App() {
-  return 
+  return <>
+  <Home/>
+  </>
 }
 
 export default App
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React from "react";
+import Typical from 'typical';
+
 
 export default function Home() {
   return (
-    <div className='profile-container'>
-      <div className='profile-parent'>
-        <div className='profile-details'>
-          <div className='colz'>
-            
+    <div className="home-container">
+      <div className="home-parent">
+        <div className="home-details">
+          <div className="colz">
+            <a href="https://www.facebook.com/">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="https://www.google.com">
+              <i className="fa fa-google-plus-square"></i>
+            </a>
+            <a href="https://www.linkedin.com">
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+            <a href="https://www.instagram.com">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="https://www.github.com/DATLARAVINDRABHUPATHIVARMA">
+              <i className="fa fa-github-square"></i>
+            </a>
+            <a href="https://www.twitter.com">
+              <i className="fa fa-twitter"></i>
+            </a>
+          </div>
+          <div className="home-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">DATLA RAVINDRA BHUPATHI VARMA</span>
+            </span>
+          </div>
+          <div className="home-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+              <Typical/>
+              </h1>
+            </span>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
