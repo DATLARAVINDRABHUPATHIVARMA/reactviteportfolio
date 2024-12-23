aacf3113e86c430ed6384bc03954a1e94d5cb719
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2024-12-23 23:31:12 +0530

    About

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import H1 from './Components/Home/H1'
+import AboutMe from './Components/AboutMe/AboutMe'
 
 
 
 function App() {
   return <>
   <H1/>
+  <AboutMe/>
   </>
 }
 
---
+++ src/Components/AboutMe/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AboutMe.css'
+
+export default function AboutMe() {
+  return (
+    <div className='aboutme-container'>
+      <div className='aboutme-parent'>
+        <h1>About Me</h1>
+      </div>
+    </div>
+  )
+}
